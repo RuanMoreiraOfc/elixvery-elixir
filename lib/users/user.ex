52400e2fc2001f5079afb2788792ee485c1b3c86
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-12 13:20:07 -0300

    feat: add `address` field

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,12 +1,13 @@
 defmodule Elixvery.Users.User do
-  @keys [:name, :email, :cpf, :age]
+  @keys [:name, :email, :address, :cpf, :age]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build(name, email, cpf, age) when is_bitstring(cpf) and age >= 18 do
+  def build(name, email, address, cpf, age) when is_bitstring(cpf) and age >= 18 do
     struct = %__MODULE__{
       name: name,
+      address: address,
       email: email,
       cpf: cpf,
       age: age
@@ -15,5 +16,5 @@ defmodule Elixvery.Users.User do
     {:ok, struct}
   end
 
-  def build(_name, _email, _cpf, _age), do: {:error, "Invalid parameters!"}
+  def build(_name, _email, _address, _cpf, _age), do: {:error, "Invalid parameters!"}
 end
