c99a37fa7c7c6e0ec2e4a935d1b1f1b89b23f0d6
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-17 15:20:08 -0300

    chore: create `Order` test factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Elixvery.Factory do
   use ExMachina
 
-  alias Elixvery.Orders.Item
+  alias Elixvery.Orders.{Item, Order}
   alias Elixvery.Users.User
 
   def user_factory do
@@ -20,6 +20,23 @@ defmodule Elixvery.Factory do
       category: :pizza,
       unit_price: Decimal.new("35.5"),
       quantity: 1
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "St Any Street",
+      items: [
+        build(:item),
+        build(:item,
+          category: :japonese_food,
+          description: "Temaki",
+          quantity: 2,
+          unit_price: Decimal.new("20.5")
+        )
+      ],
+      total_price: Decimal.new("76.50"),
+      user_cpf: "12345678900"
     }
   end
 end
