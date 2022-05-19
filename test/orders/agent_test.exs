5829001d63f493d0b3ceb7cf51e03e70520fa85c
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-19 00:59:15 -0300

    test: add "save the order in `OrderAgent`" case

--- test/orders/agent_test.exs
+++ test/orders/agent_test.exs
@@ -3,6 +3,8 @@ defmodule Elixvery.Orders.AgentTest do
 
   alias Elixvery.Orders.Agent, as: OrderAgent
 
+  import Elixvery.Factory
+
   describe "start_link/1" do
     test "starts agent and ensures PID is alive" do
       expected_response = :ok
@@ -11,6 +13,18 @@ defmodule Elixvery.Orders.AgentTest do
 
       assert response == expected_response
       assert Process.alive?(pid) == true
+    end
+  end
+
+  describe "save/1" do
+    test "saves the order in agent" do
+      order = build(:order)
+      expected_response = :ok
+
+      OrderAgent.start_link(nil)
+      {response, _uuid} = OrderAgent.save(order)
+
+      assert response == expected_response
     end
   end
 end
