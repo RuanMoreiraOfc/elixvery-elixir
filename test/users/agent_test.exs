07eac0f855977b8f223e3d99763620a7e8e69780
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-19 00:03:48 -0300

    test: add "save the user in `UserAgent`" case

--- test/users/agent_test.exs
+++ test/users/agent_test.exs
@@ -3,6 +3,8 @@ defmodule Elixvery.Users.AgentTest do
 
   alias Elixvery.Users.Agent, as: UserAgent
 
+  import Elixvery.Factory
+
   describe "start_link/1" do
     test "starts agent and ensures PID is alive" do
       expected_response = :ok
@@ -11,6 +13,18 @@ defmodule Elixvery.Users.AgentTest do
 
       assert response == expected_response
       assert Process.alive?(pid) == true
+    end
+  end
+
+  describe "save/1" do
+    test "saves the user in agent" do
+      user = build(:user)
+      expected_response = {:ok, user.cpf}
+
+      UserAgent.start_link(nil)
+      response = UserAgent.save(user)
+
+      assert response == expected_response
     end
   end
 end
