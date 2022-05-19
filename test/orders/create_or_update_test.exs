9ac6b7416798351f6ae16cf37976f4951abf45cd
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-19 01:46:06 -0300

    test: add "saves the order in agent from interface when all parameters are valid" case

---
+++ test/orders/create_or_update_test.exs
@@ -0,0 +1,39 @@
+defmodule Elixvery.Orders.CreateOrUpdateTest do
+  use ExUnit.Case
+
+  import Elixvery.Factory
+
+  alias Elixvery.Orders.Agent, as: OrderAgent
+  alias Elixvery.Orders.CreateOrUpdate, as: CreateOrUpdateOrder
+  alias Elixvery.Users.Agent, as: UserAgent
+
+  describe "call/1" do
+    setup do
+      user = build(:user)
+      item1 = build(:item)
+      item2 = build(:item, description: "Atum Pizza", quantity: 8)
+
+      UserAgent.start_link(nil)
+      UserAgent.save(user)
+      OrderAgent.start_link(nil)
+
+      {:ok, user_cpf: user.cpf, item1: item1, item2: item2}
+    end
+
+    test "saves the order in agent when all parameters are valid", %{
+      user_cpf: userCpf,
+      item1: item1,
+      item2: item2
+    } do
+      expected_response = :ok
+
+      {response, _uuid} =
+        CreateOrUpdateOrder.call(%{
+          user_cpf: userCpf,
+          items: [item1, item2]
+        })
+
+      assert response == expected_response
+    end
+  end
+end
