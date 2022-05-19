390e1b5b40d67e20461b1a4cce58ba1186c7b444
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-19 20:42:15 -0300

    test: add "`Orders.Report` create a csv file with custom path" case

--- test/orders/report_test.exs
+++ test/orders/report_test.exs
@@ -6,6 +6,7 @@ defmodule Elixvery.Orders.ReportTest do
   alias Elixvery.Orders.Agent, as: OrderAgent
   alias Elixvery.Orders.Report
 
+  # DISCLAIMER: OrderAgent.save is a async command and sometimes save in wronsg order
   describe "create/1" do
     setup do
       ensure_there_is_no_file =
@@ -45,7 +46,36 @@ defmodule Elixvery.Orders.ReportTest do
 
       response = File.read!(file_path)
       File.rm!(file_path)
-      # DISCLAIMER: OrderAgent.save is a async command and sometimes save in wronsg order
+      assert response == expected_response1 or response == expected_response2
+    end
+
+    test "creates the report with custom path", %{
+      ensure_there_is_no_file: ensure_there_is_no_file
+    } do
+      file_path = "test/report_test.csv"
+
+      orders = [
+        build(:order),
+        build(:order, user_cpf: "00987654321")
+      ]
+
+      expected_response1 = """
+      00987654321,pizza,1,35.5,japonese_food,2,20.5,76.50
+      12345678900,pizza,1,35.5,japonese_food,2,20.5,76.50
+      """
+
+      expected_response2 = """
+      12345678900,pizza,1,35.5,japonese_food,2,20.5,76.50
+      00987654321,pizza,1,35.5,japonese_food,2,20.5,76.50
+      """
+
+      ensure_there_is_no_file.(file_path)
+      OrderAgent.start_link(nil)
+      Enum.each(orders, &OrderAgent.save/1)
+      Report.create(file_path)
+
+      response = File.read!(file_path)
+      File.rm!(file_path)
       assert response == expected_response1 or response == expected_response2
     end
   end
