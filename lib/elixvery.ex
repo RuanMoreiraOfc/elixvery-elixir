99c39ddd32f8451975f4b6876de21cec6bb68066
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-18 23:06:47 -0300

    feat: delegate `create_or_update_order` to main

--- lib/elixvery.ex
+++ lib/elixvery.ex
@@ -1,10 +1,14 @@
 defmodule Elixvery do
+  alias Elixvery.Orders.Agent, as: OrderAgent
+  alias Elixvery.Orders.CreateOrUpdate, as: CreateOrUpdateOrder
   alias Elixvery.Users.Agent, as: UserAgent
-  alias Elixvery.Users.CreateOrUpdate
+  alias Elixvery.Users.CreateOrUpdate, as: CreateOrUpdateUser
 
   def start_agents do
     UserAgent.start_link(nil)
+    OrderAgent.start_link(nil)
   end
 
-  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_order(params), to: CreateOrUpdateOrder, as: :call
 end
